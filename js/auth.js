2289ac80552ec6872e0515a224b635f298aa0bf7
Rozhkov Dmitriy <rozhkovdmitriy@gmail.com>
2011-07-22 01:00:26 +0400

    Поработал над версткой панели авторизации

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,4 @@
 
-$(document).ready(function() {
-	$("#auth-form").ajaxForm({url: "/process-auth", type:"POST", success:handleAuth});
-    });
-
 /** Функция обработчик, результата авторизации */
 function handleAuth(data) {
     alert(data);return;
@@ -11,7 +7,7 @@ function handleAuth(data) {
 
 }
 
-
+/** Обработка юзера после авторизации */
 function processCurrentUser() {
     if (!document.currentUser)
 	return;
@@ -19,3 +15,21 @@ function processCurrentUser() {
     alert(cu.name + ' ' + cu.password);
     $("#adminMenuItem").css("display", cu.type <= 2?"list-item":"none");
 }
+
+var authPanelVisible = true;
+function toggleAuthPanel() {
+    $("#authPanel").animate({ top: (authPanelVisible?"-=44px":"+=44px"), }, 'slow' );
+    authPanelVisible = !authPanelVisible;
+    $("body").css("padding-top", authPanelVisible?"55px":"10px");
+    $("div#toggleAuth img").attr("src", authPanelVisible?"img/up.png":"img/down.png");
+    
+}
+
+
+$(document).ready(function() {
+	$("#auth-form").ajaxForm({url: "/process-auth", type:"POST", success:handleAuth});
+	$("#authPanel").click(toggleAuthPanel);
+	toggleAuthPanel();
+	
+    });
+

f7d4026a6ae24d8b32f556f009ba858b0dcfd2e8
Rozhkov Dmitriy <rozhkovdmitriy@gmail.com>
2011-08-13 16:45:00 +0400

    init modified a bit for release

--- init.lisp
+++ init.lisp
@@ -1,20 +1,25 @@
-(require 'hunchentoot)
-(require 'cl-who)
-(require 'trivial-shell)
-(require 'cl-ppcre)
-(require 'cl-json)
-(require 'alexandria)
-(require 'mongo-cl-driver)
-;(require 'yason)
-;(require 'yason)
-*;(require 'hunchentoot-test)
-(defpackage :webserver
+defpackage :webserver
   (:use :common-lisp :hunchentoot :cl-who
 	:mongo :mongo-cl-driver.son-sugar
 	:trivial-shell :cl-ppcre :json :alexandria))
 
 (in-package :webserver)
 
+(require 'hunchentoot)
+(require 'cl-who)
+(require 'trivial-shell)
+(require 'cl-ppcre)
+(require 'cl-json)
+(require 'alexandria)
+
+;;push (truename #P"/home/rozhkovdmitriy/lisp-packets/mongo-cl-driver/") asdf:*central-registry*)
+(require 'mongo-cl-driver)
+;(require 'yason)
+;(require 'yason)
+*;(require 'hunchentoot-test)
+(
 (defparameter +root-path+ #p"/home/rozhkovdmitriy/ski73/")
 (defparameter +tmp-relative-path+ #p"tmp")
 (defparameter +tmp-path+ (merge-pathnames +tmp-relative-path+ +root-path+))
+
+;;(load (merge-pathnames "webserver.lisp" +root-path+))
